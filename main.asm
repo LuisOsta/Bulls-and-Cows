cca0a6b8ba56905b6d9d9479ce52179d327c38f5
Luis Osta <luisosta1999@gmail.com>
2018-10-17 13:18:34 -0500

    added welcome prompt, and other user input prompts, finished outputText function

--- main.asm
+++ main.asm
@@ -1,6 +1,12 @@
 	.data
 test: .asciiz "Test Output"
-.globl test
+
+#welcome, basic messages
+welcomePrompt: .asciiz "We have chosen a 4 letter, word. Let the game begin!"
+
+# user input prompts
+guessPrompt: .asciiz "What will your guess be: "
+giveupPrompt: .asciiz "Do you wish to give up or continue"
 
 #utlity newline
 newline: .asciiz "\n"
@@ -11,7 +17,8 @@ outputEnd: .asciiz "Goodbye. The program has ended."
 
 	.text
 main:
-	
+	la $a0, welcomePrompt
+	jal outputText
 	
 	Exit:
 		li, $v0, 4
--- utils.asm
+++ utils.asm
@@ -1,7 +1,13 @@
 	.text
 .globl outputText
 
+# The text to output will be on a0
 outputText:
-
+	#prints out input
+	li, $v0, 4
+	syscall
+	#prints out a newline
+	la, $a0, newline
+	syscall
 	jr $ra
 	

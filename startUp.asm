65494b8c33c4cffcae05139d01366d9a42114c41
Luis Osta <luisosta1999@gmail.com>
2018-11-15 21:04:53 -0600

    Completed substring functionality, and random word selection works

--- startUp.asm
+++ startUp.asm
@@ -2,34 +2,46 @@
 buffer: .space 1024
 filename: .asciiz "dictionary.txt"
 wordNumber: .word 100
-
+newline: .asciiz "\n"
+word: .asciiz "xxxx"
 	.text
 main:
-	jal getRandomInt
-	move $t0, $v0 #the random word(row r)
-	move $t1, $t0#the words leading up to the word(row r -1)
+	jal getRandomInts
+	move $s0, $v0 #the random word(row r) multiplied by 4
+	move $s1, $v1#the words leading up to the word(row r -1) multiplied by 4; used for the substring
 	
-	sub $t1, $t1, 1
-	
-	mul $t0,$t0, 4 #(multiplies it by the word length)
-	mul $t1, $t1, 4 # as above
-	
+	#################################################
+	#PRINTS OUT THE RANDOM INDEX OF THE WORD TO USE
 	li $v0, 1
-	la $a0, ($t0)
+	la $a0, ($s0) #prints out the random integer
 	syscall
 	
- 	jal openFile
-  	move $s6, $v0      # save the file descriptor 
-	move $a0, $s6	#moves the file descriptor
-	move $a2, $t0 #moves the word byte start
-	
-	jal pickRandomWord
- 
-	la $a0, buffer
+	la $a0, newline
 	li $v0, 4
 	syscall
 	
-	la $t0, buffer
+	
+	################################################
+	#OPENS THE DICTIONARY FILE
+ 	jal openFile
+  	move $s6, $v0      # save the file descriptor
+ 	
+ 	################################################
+ 	
+ 	
+ 	###############################################
+ 	#LOADS THE BUFFER WITH ALL WORDS LEADING UP(INCLUSIVE) WORD: AND THEN RETURNS THE SUBSTRINGED RESULT
+	move $a0, $s6	#moves the file descriptor
+	move $a2, $s0 #moves the word byte start
+	jal pickRandomWord #the buffer holds the string
+	
+	move $a0, $s1
+	sub $a1, $s0, $s1
+	jal substringBuffer # v0, v1, v2, v3, ..v_n contain the letters of the word(for our case it will always be 4  letters)
+	
+	la  $a0, word
+	li $v0, 4
+	syscall
 	Exit:
 	  ###############################################################
   		# Close the file 
@@ -40,13 +52,32 @@ main:
 		li $v0, 10
 		syscall
 
+substringBuffer: #receives the starting index in #a0, and final index in $a1, returns the substringed version of the buffer
+	li $t1, 0 #counter
+	move $t2, $a1 # the word length(equal to 4 in our case)
+	la $t3, buffer
+	
+	addu $t4, $t3, $a0  # BUFFER POINTER sets t4 to the base string value  assumes x is in $a0
+	Loop:
+		beq $t1, $t2, LoopExit
+	
+		#lb $v0,($t4)      # read the 1st character
+		lb $t0, ($t4) #loads the character in the buffer
+		sb $t0, word($t1) #appends it to the corresponding index of the word
+		
+		addu $t4, $t4, 1 #adds the counter to the buffer pointer
+		addi $t1, $t1, 1 #adds 1 to the counter
+		j Loop
+	LoopExit:
+		
+		jr $ra
+	
 pickRandomWord:
 	###############################################################
   	# Read from file just opened
   	li   $v0, 14       # system call to read from file
   	move $a0, $s6      # file descriptor 
   	la   $a1, buffer   # address of buffer from which to write
-  	#la   $a2, 372      # buffer length in a2
   	syscall
   	
 openFile:
@@ -56,13 +87,18 @@ openFile:
   	la   $a0, filename     # output file name
   	li   $a1, 0        # Open for writing (flags are 0: read, 1: write)
   	li   $a2, 0        # mode is ignored
+  	
   	syscall            # open a file (file descriptor returned in $v0)
   	jr $ra      	# returns the file descriptor
   	
-getRandomInt:
+getRandomInts:
 	lw $a1, wordNumber
     	li $v0, 42   #random
     	syscall
-   	
+    	
+   	sub $v1, $a0, 1
    	move $v0, $a0
+   	
+   	mul $v0,$v0, 6 #(multiplies it by the word length)
+	mul $v1, $v1, 6 # as above
    	jr $ra
